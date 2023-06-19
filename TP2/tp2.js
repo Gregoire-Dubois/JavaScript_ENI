b0d0d5b3644f0ee66669b2a8952f71b45bdf84f2
Gregoire-Dubois <65712692+Gregoire-Dubois@users.noreply.github.com>
2023-06-19 17:24:53 +0200

    début TP2 mise en place de la communication entre le DOM et le formulaire

--- TP2/tp2.js
+++ TP2/tp2.js
@@ -3,12 +3,16 @@ function enregistrer()
     var nom = document.getElementById("auteur");
     var savoir = document.getElementById("savoir");
     var date = document.getElementById("datePublication");
+
+    var tableauSaisies =[];
+
     var rstHtml = `<p>savoir : ${savoir.value}</p> 
                           <p>nom : ${nom.value}</p> 
                           <p>date : ${date.value}</p>`
-
     document.getElementById('resultat').innerHTML = rstHtml;
     return rstHtml;
 
 }
+
+
 
