83d59817454b452e5026b925d7d2971e229a6b69
Gregoire-Dubois <65712692+Gregoire-Dubois@users.noreply.github.com>
2023-06-20 13:14:28 +0200

    TP 2 terminé

--- TP2/tp2.js
+++ TP2/tp2.js
@@ -1,18 +1,50 @@
-function enregistrer()
-{
-    var nom = document.getElementById("auteur");
-    var savoir = document.getElementById("savoir");
-    var date = document.getElementById("datePublication");
+const form = document.getElementById("form");
+const list = document.getElementById("list")
 
-    var tableauSaisies =[];
+form.date.valueAsDate = new Date();
+form.author.focus();
 
-    var rstHtml = `<p>savoir : ${savoir.value}</p> 
-                          <p>nom : ${nom.value}</p> 
-                          <p>date : ${date.value}</p>`
-    document.getElementById('resultat').innerHTML = rstHtml;
-    return rstHtml;
+form.addEventListener("submit", function (e){
+    e.preventDefault();
 
-}
+    /*
+    Créer les conteneurs pour le HTML et afficher la liste
+     */
+    const li = document.createElement("li");
+    const text = document.createElement("div");
+    const details = document.createElement("div");
+
+    /*
+    Affecter la valeur du formulaire aux variables
+     */
+    text.innerText = form.text.value;
+    details.innerText = form.author.value + " " + form.date.valueAsDate;
+
+    /*
+    Attacher les valeurs récupérées dans l'objet enfant
+     */
+    li.appendChild(text);
+    li.appendChild(details);
+    list.appendChild(li);
+
+    li.addEventListener("click", function (e){
+        console.log(e.currentTarget);
+        /*
+        dans le lli on récupère le 1er enfant
+         */
+        const  text  = e.currentTarget.children[0].innerText
+        confirm("Voulez vous supprimer " + text);
+
+        /*
+        si la persnne valide
+         */
+        /*
+        On récupère le parent pour supprimer l'enfant
+         */
+        e.currentTarget.parentNode.removeChild(e.currentTarget);
 
 
 
+    });
+
+});
