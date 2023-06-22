84727e9747e95a797017420999d1f5f5ab60f35e
Gregoire-Dubois <65712692+Gregoire-Dubois@users.noreply.github.com>
2023-06-22 13:16:31 +0200

    TP 3 ajout fonctionnalité de trie par ordre alphabetique et par  dates. fonctionne en console

--- TP3/tp3.js
+++ TP3/tp3.js
@@ -4,6 +4,7 @@ class SavoirInutile {
         this.form = document.getElementById(formId);
         this.list = document.getElementById(listId);
         this.initializeForm();
+        this.selectOrder();
     }
 
     initializeForm() {
@@ -22,8 +23,7 @@ class SavoirInutile {
         const details = document.createElement("div");
 
         text.innerText = this.form.text.value;
-        details.innerText =
-            this.form.author.value + " " + this.form.date.valueAsDate;
+        details.innerText = this.form.author.value + " " + this.form.date.valueAsDate;
 
         li.appendChild(text);
         li.appendChild(details);
@@ -35,8 +35,57 @@ class SavoirInutile {
                 e.currentTarget.parentNode.removeChild(e.currentTarget);
             }
         });
-    }
-}
 
+    }
+
+     selectOrder(){
+         const tableauSavoirInutile = []; // Déplacez la déclaration du tableau ici pour qu'il soit accessible dans toute la méthode.
+         const selecteur = document.getElementById("selectOrder");
+
+             selecteur.addEventListener("change", (event) => {
+
+                 if (event.target.value === "alphabétique") {
+
+                     const compareNoms = (a, b) => {
+                        const  nomA = a.auteur.toLowerCase();
+                        const  nomB = b.auteur.toLowerCase();
+                        if (nomA< nomB){return -1};
+                        if (nomA > nomB){return 1};
+                        return 0;
+                    }
+                    tableauSavoirInutile.sort(compareNoms);
+
+                    console.log(tableauSavoirInutile);
+
+
+                 } else {
+                    const compareDates = (a ,b) => {
+                        const dateA = new Date(a.date).getTime();
+                        const dateB = new Date(b.date).getTime();
+                        if(dateA > dateB){return -1}
+                        if (dateA < dateB){return 1}
+
+                        return 0
+                    }
+                     tableauSavoirInutile.sort(compareDates);
+                     console.log(tableauSavoirInutile);
+
+                 }
+             });
+
+             this.form.addEventListener("submit", (e) => {
+                 e.preventDefault();
+                 const objetSavoirInutile = {
+                     date: this.form.date.value,
+                     auteur: this.form.author.value,
+                     savoir: this.form.text.value
+                 };
+                 tableauSavoirInutile.push(objetSavoirInutile);
+                 console.log(tableauSavoirInutile);
+             });
+         }
+}
 // Utilisation de la classe SavoirInutile
 const savoirInutile = new SavoirInutile("form", "list");
+
+
