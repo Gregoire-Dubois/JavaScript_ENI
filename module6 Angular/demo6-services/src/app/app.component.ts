ff60a99f249a9ca4e12d858b97d138b1d243499b
Gregoire-Dubois <65712692+Gregoire-Dubois@users.noreply.github.com>
2023-06-27 14:05:20 +0200

    début module 6 avec Angular - les services

---
+++ module6 Angular/demo6-services/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {ReglesMetiersService} from "./services/regles-metiers.service";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'demo6-services';
+  nombres : number[] = [1, 3, 8, 15, 16];
+
+  constructor(private reglesMetiersServices : ReglesMetiersService) { }
+
+  getClasses(nombre : number){
+    const estPaire = this.reglesMetiersServices.estPaire(nombre);
+    return {'vert' : estPaire, 'rouge' : !estPaire};
+  }
+
+}
