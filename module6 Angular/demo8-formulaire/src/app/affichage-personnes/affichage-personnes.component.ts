189ba51d24062e0e6252b217963f141daf3b79c8
Gregoire-Dubois <65712692+Gregoire-Dubois@users.noreply.github.com>
2023-06-28 13:26:59 +0200

    module6 - les formulaires (terminé)

--- module6 Angular/demo8-formulaire/src/app/affichage-personnes/affichage-personnes.component.ts
+++ module6 Angular/demo8-formulaire/src/app/affichage-personnes/affichage-personnes.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Personne} from "../models/personne";
+import {GestionPersonnesService} from "../services/gestion-personnes.service";
 
 @Component({
   selector: 'app-affichage-personnes',
   templateUrl: './affichage-personnes.component.html',
   styleUrls: ['./affichage-personnes.component.css']
 })
-export class AffichagePersonnesComponent {
+export class AffichagePersonnesComponent implements OnInit, OnDestroy{
+
+  personnes : Personne[] =[];
+
+  constructor(private gestionPersonne: GestionPersonnesService) {
+
+  }
+
+  ngOnInit(): void {
+    this.gestionPersonne.emmeteurPersonnes.subscribe( (value) => this.personnes = value);
+  }
+
+  ngOnDestroy(): void {
+    this.gestionPersonne.emmeteurPersonnes.unsubscribe();
+  }
+
 
 }
